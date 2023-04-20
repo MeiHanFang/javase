127bb29a2d3093cc4bb64ac9f1d4a02e9fadc67b
hainiu <hainiu@example.com>
2023-04-20 20:30:28 +0800

    Revert "javase阶段代码 增加了一个Demo.java文件"
    This reverts commit 79b8ebee

--- day18-mvntest1/src/main/java/com/hainiu/Demo.java
+++ day18-mvntest1/src/main/java/com/hainiu/Demo.java
@@ -5,7 +5,5 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("java");
-        System.out.println("java");
-        System.out.println("java");
     }
 }
